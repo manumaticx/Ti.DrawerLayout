cca74bac2fd0224d0c9fcbb4749f098f9c500780
Manuel Lehner <manumaticx@gmail.com>
2014-06-25 11:30:07 +0200

    make Fragment non-static standalone class

---
+++ src/com/tripvi/drawerlayout/ContentWrapperFragment.java
@@ -0,0 +1,42 @@
+package com.tripvi.drawerlayout;
+
+import org.appcelerator.titanium.util.TiRHelper;
+import org.appcelerator.titanium.util.TiRHelper.ResourceNotFoundException;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout.LayoutParams;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public class ContentWrapperFragment extends Fragment {
+	
+	private static final String TAG = "TripviContentFragment";
+	
+	View mContentView;
+	
+	public ContentWrapperFragment(View cv) {
+		mContentView = cv;
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		int layout_content_fragment = 0;
+		int id_content_frame = 0;
+		try {
+			layout_content_fragment = TiRHelper.getResource("layout.content_fragment");
+			id_content_frame = TiRHelper.getResource("id.content_frame");
+		}
+		catch (ResourceNotFoundException e) {
+			Log.e(TAG, "XML resources could not be found!!!");
+		}
+		
+		View view = inflater.inflate(layout_content_fragment, container, false);
+		FrameLayout v = (FrameLayout) view.findViewById(id_content_frame);
+		v.addView(mContentView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		return view;
+	}
+}
--- src/com/tripvi/drawerlayout/Drawer.java
+++ src/com/tripvi/drawerlayout/Drawer.java
@@ -10,8 +10,6 @@ import org.appcelerator.titanium.util.TiRHelper.ResourceNotFoundException;
 import org.appcelerator.titanium.view.TiUIView;
 
 import ti.modules.titanium.ui.WindowProxy;
-
-import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -22,7 +20,6 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 
@@ -256,32 +253,6 @@ public class Drawer extends TiUIView {
 		// fragmentManager.beginTransaction().replace(id_content_frame, fragment).commitAllowingStateLoss();
 		
 		this.centerView = viewProxy;
-	}
-	
-	public static class ContentWrapperFragment extends Fragment {
-		View mContentView;
-		
-		public ContentWrapperFragment(View cv) {
-			mContentView = cv;
-		}
-		
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			int layout_content_fragment = 0;
-			int id_content_frame = 0;
-			try {
-				layout_content_fragment = TiRHelper.getResource("layout.content_fragment");
-				id_content_frame = TiRHelper.getResource("id.content_frame");
-			}
-			catch (ResourceNotFoundException e) {
-				Log.e(TAG, "XML resources could not be found!!!");
-			}
-			
-			View view = inflater.inflate(layout_content_fragment, container, false);
-			FrameLayout v = (FrameLayout) view.findViewById(id_content_frame);
-			v.addView(mContentView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-			return view;
-		}
 	}
 	
 	
